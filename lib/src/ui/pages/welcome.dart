45e2380d8995086fd75ccf86d226b39023c35118
Luis Guillermo Perez Rodriguez <luisguillermo.rodriguez32@gmail.com>
2022-12-13 00:45:35 -0500

    Feat: Welcome page finished

--- lib/src/ui/pages/welcome.dart
+++ lib/src/ui/pages/welcome.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fortour/src/ui/pages/home.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -12,7 +14,7 @@ class WelcomePage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: size.height * .6,
+            height: size.height * .55,
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.vertical(
                 bottom: Radius.circular(30),
@@ -26,31 +28,57 @@ class WelcomePage extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Es Verano?\nVen con Nostros!!!',
+              children: [
+                const Text(
+                  'Es Verano?\nVen con Nosotros!!!',
                   textAlign: TextAlign.start,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 36,
+                    fontSize: 28,
                   ),
                 ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final.',
+                  textAlign: TextAlign.start,
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () =>
+                        Navigator.of(context).popAndPushNamed(HomePage.name),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Vámonos'),
+                        SizedBox(width: 10),
+                        Icon(CupertinoIcons.arrow_right),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
         ],
       ),
-      // body: Center(
-      //   child: MaterialButton(
-      //     onPressed: () => Navigator.of(context).popAndPushNamed(HomePage.name),
-      //     color: Theme.of(context).primaryColor,
-      //     child: const Text('Home'),
-      //   ),
-      // ),
     );
   }
 }
