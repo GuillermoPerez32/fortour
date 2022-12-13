6be2e02164d0d1da7d6574d3cce11bd90bc8fe31
Luis Guillermo Perez Rodriguez <luisguillermo.rodriguez32@gmail.com>
2022-12-13 00:20:25 -0500

    Feat: adding welcome page

--- lib/src/ui/pages/welcome.dart
+++ lib/src/ui/pages/welcome.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fortour/src/ui/pages/home.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -8,17 +7,50 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome'),
+      body: Column(
+        children: [
+          Container(
+            height: size.height * .6,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/images/welcome.jpg',
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Es Verano?\nVen con Nostros!!!',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () => Navigator.of(context).popAndPushNamed(HomePage.name),
-          color: Theme.of(context).primaryColor,
-          child: const Text('Home'),
-        ),
-      ),
+      // body: Center(
+      //   child: MaterialButton(
+      //     onPressed: () => Navigator.of(context).popAndPushNamed(HomePage.name),
+      //     color: Theme.of(context).primaryColor,
+      //     child: const Text('Home'),
+      //   ),
+      // ),
     );
   }
 }
